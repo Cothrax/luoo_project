bb8c89ef4c75fc81ed1502537013f4ff1e2de7a4
Cothrax <clarklee.me@gmail.com>
2019-08-12 09:52:37 +0800

    fifth commit

--- luoo_init/scripts/190812_update_tag.py
+++ luoo_init/scripts/190812_update_tag.py
@@ -4,4 +4,21 @@ client = Elasticsearch(hosts=['127.0.0.1'], timeout=20)
 
 def update_tag():
     """清除[EMPTY]的tag"""
-    client.update_by_query
+    client.update_by_query(
+        index='luoo1',
+        body={
+            "script": {
+                "source": "ctx._source.tag=''",
+                "lang": "painless"
+            },
+            "query": {
+                "match": {
+                    "tag": "empty"
+                }
+            }
+        }
+    )
+
+
+if __name__ == '__main__':
+    update_tag()
